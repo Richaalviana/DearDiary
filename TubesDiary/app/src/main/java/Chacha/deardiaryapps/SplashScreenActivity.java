dd2374f263a3ad2e5575d50714feff87685a44d6
Richa Alvianawati S <32487705+Richaalviana@users.noreply.github.com>
2019-08-01 09:45:09 +0700

    Add files via upload

---
+++ TubesDiary/app/src/main/java/Chacha/deardiaryapps/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package Chacha.deardiaryapps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Menunggu 3 seconds kemudian membuka MainActivity.class
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
+
